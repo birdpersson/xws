6a08ca9a4febc56a8baff4666c96dd760808b7a6
birdpersson <aleksandarvujasin@gmail.com>
2021-03-15 20:17:00 +0100

    Configure Spring Security

---
+++ src/main/java/com/bsep12/bsep/service/UserService.java
@@ -0,0 +1,29 @@
+package com.bsep12.bsep.service;
+
+import com.bsep12.bsep.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+
+}
