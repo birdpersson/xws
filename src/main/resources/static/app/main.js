fc529699d554cb896cf445d0f174b9772ebefaab
birdpersson <aleksandarvujasin@gmail.com>
2021-03-18 16:52:07 +0100

    Add login

--- src/main/resources/static/app/main.js
+++ src/main/resources/static/app/main.js
@@ -1,13 +1,13 @@
-const routes = [
+const Login = { template: "<login></login>" };
 
-]
+const routes = [
+	{ path: "/login", component: Login },
+];
 
 const router = new VueRouter({
 	routes
 });
 
-axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('jwt');
-
 const app = new Vue({
 	router,
-}).$mount('#app');
+}).$mount("#app");
