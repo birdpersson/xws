d1038fde7aac583558ababef840543532ababa7c
birdpersson <aleksandarvujasin@gmail.com>
2021-03-19 13:23:35 +0100

    Add create cert page

--- src/main/resources/static/app/main.js
+++ src/main/resources/static/app/main.js
@@ -1,13 +1,17 @@
 const Login = { template: "<login></login>" };
+const Create = { template: "<create></create>" };
 
 const routes = [
-	{ path: "/login", component: Login },
+  { path: "/login", component: Login },
+  { path: "/create", component: Create },
 ];
 
 const router = new VueRouter({
-	routes
+  routes
 });
 
+axios.defaults.headers.common["Authorization"] = "Bearer " + localStorage.getItem("accessToken");
+
 const app = new Vue({
-	router,
+  router,
 }).$mount("#app");
