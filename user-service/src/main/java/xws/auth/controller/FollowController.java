26e5d4ef3e3bee3d593f2cfaf5d0cb0ab841a99a
birdpersson <aleksandarvujasin@gmail.com>
2021-06-07 17:13:06 +0200

    Add following

--- user-service/src/main/java/xws/auth/controller/FollowController.java
+++ user-service/src/main/java/xws/auth/controller/FollowController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xws.auth.security.TokenUtils;
 import xws.auth.service.UserService;
@@ -13,7 +12,6 @@ import xws.auth.service.UserService;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping(value = "/follow")
 public class FollowController {
 
 	@Autowired
@@ -22,10 +20,16 @@ public class FollowController {
 	@Autowired
 	UserService userService;
 
-	@PostMapping("/{username}")
+	@PostMapping("/follow/{username}")
 	public ResponseEntity followUser(@PathVariable("username") String subjectUsername, HttpServletRequest request) {
 		String issuerUsername = tokenUtils.getUsernameFromToken(tokenUtils.getToken(request));
 		return new ResponseEntity(userService.followUser(issuerUsername, subjectUsername), HttpStatus.CREATED);
+	}
+
+	@PostMapping("/accept/{username}")
+	public ResponseEntity acceptFollowing(@PathVariable("username") String issuerUsername, HttpServletRequest request) {
+		String subjectUsername = tokenUtils.getUsernameFromToken(tokenUtils.getToken(request));
+		return new ResponseEntity(userService.acceptFollower(issuerUsername, subjectUsername), HttpStatus.CREATED);
 	}
 
 }
