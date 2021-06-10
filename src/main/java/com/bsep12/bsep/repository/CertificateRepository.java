d76049bcf718061ce38cdc65df2ffb0d5ebe5104
birdpersson <aleksandarvujasin@gmail.com>
2021-06-10 12:02:51 +0200

    Remove duplicates

--- src/main/java/com/bsep12/bsep/repository/CertificateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bsep12.bsep.repository;
-
-import com.bsep12.bsep.model.Certificate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CertificateRepository extends JpaRepository<Certificate, Long> {
-}
