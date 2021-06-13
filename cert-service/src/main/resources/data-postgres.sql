e9980e98b57b05ad225abda8c650662f45c21e9f
birdpersson <aleksandarvujasin@gmail.com>
2021-06-13 03:05:39 +0200

    Test https docker

--- cert-service/src/main/resources/data-postgres.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO USERS (username, password, enabled) VALUES ('admin', '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', true);
-
-INSERT INTO AUTHORITY (name) VALUES ('ROLE_ADMIN');
-
-INSERT INTO USER_AUTHORITY (user_id, authority_id) VALUES (1, 1);
